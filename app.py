4086c1f0abe026f7b513cb762c7f646408b251c5
siva gopal <sivagopal.b@gmail.com>
2019-07-03 11:52:31 +0100

    test flask

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ def hello_user(username):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')     # open for everyone
-
-if __name__ == '__main__':
     ############# Add these lines #############
     import xmlrunner
     runner = xmlrunner.XMLTestRunner(output='test-reports')
